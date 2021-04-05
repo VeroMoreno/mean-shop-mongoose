a143fe933af6bd01b2770f87487628c8b731c365
Veronica <veronica.moreno.work@gmail.com>
2021-04-05 09:58:17 +0200

    localStorage on fire in session service

--- src/app/servicios/sessionService.ts
+++ src/app/servicios/sessionService.ts
@@ -2,20 +2,36 @@ import { Injectable } from "@angular/core"
 
 @Injectable({ providedIn: 'root' })
 export class SessionService {
-  private items: [] = []
-  public setItem(clave:string, valor:any):void {
-    this.items[clave] = valor
+  // Para guardar objetos en memoria, cada vez que cambiamos cualquier cosa, todo desaparecía
+  // Ahora con sessionStorage, podemos conservar cualquier cosa.
+
+  // private items: [] = []
+
+  public constructor() {
+    console.log("creando session service")
+  }
+
+  public setItem(clave:string, valor:any, persistente:boolean = false):void {
+    // this.items[clave] = valor
+    if(persistente) {
+      localStorage.setItem(clave, JSON.stringify(valor))
+    } else {
+      sessionStorage.setItem(clave, JSON.stringify(valor))
+    }
   }
 
   public getItem(clave:string):any {
-    return this.items[clave]
+    // return this.items[clave]
+    return JSON.parse(sessionStorage.getItem(clave))
   }
 
   public removeItem(clave:string):void {
-    delete this.items[clave]
+    // delete this.items[clave]
+    sessionStorage.removeItem(clave)
   }
 
   public clear():void {
-    this.items = []
+    //this.items = []
+    sessionStorage.clear()
   }
 }
