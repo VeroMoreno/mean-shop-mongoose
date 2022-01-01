751e416cf6347bbb20479f79ef230ba0db1ec234
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 09:53:20 +0800

    Delete src directory

--- src/app/modulos/tienda/entidades/criterioBusquedaProducto.ts
+++
@@ -1,9 +0,0 @@
-export class CriterioBusquedaProducto {
-  public constructor(
-      public texto      : string = null,
-      public categoria  : number = null,
-      public fabricante : string = null,
-      public precioMin  : string = null,
-      public precioMax  : string = null,
-  ) {}
-}
--- src/app/modulos/tienda/entidades/detallePedido.ts
+++
@@ -1,9 +0,0 @@
-import { Producto } from './producto'
-
-export class DetallePedido {
-  public constructor(
-      public cantidad : number = null,
-      public precio   : number = null,
-      public producto : Producto = null,
-  ) {}
-}
